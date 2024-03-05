8c4af9c0c039570a6eaae1fb89d84afb0b3c781e
Your Name <you@example.com>
2024-03-05 23:49:50 +0500

    first commit

--- A # 15 Blocktopia/app.js
+++ A # 15 Blocktopia/app.js
@@ -1,1 +1,1 @@
-//window.alert("This Page Is Not Original Site It Is Made For Training Purpose Developed By Naveed Ahmed");
+window.alert("This Page Is Not Original Site It Is Made For Training Purpose Developed By Naveed Ahmed");
---
+++ A # 16 Javascript 1-11/app.js
@@ -0,0 +1,24 @@
+// Javascript Assignment
+// Chapter 01
+
+// 1.
+
+// var massage = ("Greetings & Welcome To Naveed's Website");
+// alert(massage)
+
+//2
+
+// var massage = ("Error! Please Enter A Valid Password.");
+// alert(massage)
+
+//3
+
+// var massage = ("Welcome To JS Land... \nHappy Coding");
+// alert(massage)
+
+//4
+
+alert(Welcome To JS Land")
+
+
+
