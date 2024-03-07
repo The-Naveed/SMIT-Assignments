9794e4a35829b14c624506a92ee3ebb1385dc352
Your Name <you@example.com>
2024-03-07 22:44:33 +0500

    Second Commit

--- A # 16 Javascript 1-11/app.js
+++ A # 16 Javascript 1-11/app.js
@@ -1,24 +1,110 @@
 // Javascript Assignment
+
 // Chapter 01
 
-// 1.
+// 1.Write a script to greet your website visitor using JS alert box.
 
 // var massage = ("Greetings & Welcome To Naveed's Website");
 // alert(massage)
 
-//2
+//2. Write a script to display following message on your web page:
 
 // var massage = ("Error! Please Enter A Valid Password.");
 // alert(massage)
 
-//3
+//3. Write a script to display following message on your web page: (Hint : Use line break)
 
-// var massage = ("Welcome To JS Land... \nHappy Coding");
+// var massage = ("Welcome To JS Land...\nHappy Coding");
 // alert(massage)
 
-//4
+//4. Write a script to display following messages in sequence:
 
-alert(Welcome To JS Land")
+// alert ("Welcome To JS Land...")
+// alert ("Happy Coding\nPrevent This Page From Creating Additional Dialogs")
 
+// 5. Generate the following message through browser’s developer console:
 
+// var massage = ("Hello... I Can Run Javascript Through My Web Browser's Console")
+// console.log(massage)
 
+// Chapter 02
+
+// 1. Declare a variable called username.
+
+// var userName = ("Declare a variable called username.")
+
+// 2. Declare a variable called myName & assign to it a string that represents your Full Name.
+
+// var myName = ("My Name Is Naveed Ahmed")
+
+// 3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+
+// var massage = ("Hello World")
+// alert (massage)
+
+// 4. Write a script to save student’s bio data in JS variables and show the data in alert boxes.
+
+// var userName = ("Naveed Ahmed");
+// var age = ("24 Years Old");
+// var job = ("High Precision Tool Maker");
+// alert (userName)
+// alert (age)
+// alert (job)
+
+// 5. Write a script to display the following alert using one JS variable:
+
+// var display = ("PIZZA\nPIZZ\nPIZ\nPI\nP");
+// alert (display)
+
+// 6. Declare a variable called email and assign to it a string that represents your Email Address(e.g. example@example.com). Show the blow mentioned message in an alert box.(Hint: use string concatenation)
+
+// var info = ("My Email Address Is")
+// var eMail = ("naveedpstc@outlook.com")
+// alert (info+" "+eMail)
+
+// 7. Declare a variable called book & give it the value “A smarter way to learn JavaScript”. Display the following message in an alert box:
+
+// var book1 = ("I'am Trying To Learn Javascript From Book")
+// var book2 = ("A smarter way to learn JavaScript")
+// alert (book1+ " " +book2)
+
+// 8. Write a script to display this in browser through JS
+
+// document.write("Yah! I Can Write In HTML Through Javascript")
+
+// 9. Store following string in a variable and show in alert and browser through JS “▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬”
+
+// var design = ("▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬")
+// alert (design)
+
+// Chapter 03
+
+// 1. Declare a variable called age & assign to it your age. Show your age in an alert box.
+
+// var age = ("My Age Is 24")
+// alert (age)
+
+// 2. Declare & initialize a variable to keep track of how many times
+// a visitor has visited a web page. Show his/her number of visits on your
+// web page. For example: “You have visited this site N times”.
+
+// var times = ("You have visited this site 10 times")
+// alert (times)
+
+// 3. Declare a variable called birthYear & assign to it your birth year. Show the following message in your browser:
+
+// document.write("My Birth Year Is 2000<br>Data Type Of My Decleard Is Number")
+
+// 4. A visitor visits an online clothing store www.xyzClothing.com
+//Write a script to store in variables the following information:
+//a. Visitor’s name b. Product title c. Quantity i.e. how many products a
+//visitor wants to order Show the following message in your browser:
+//“John Doe ordered 5 T-shirt(s) on XYZ Clothing store”.
+
+// var userName = ("John Doe");
+// var productTitle = ("T Shirt(s)");
+// var brand = ("XYZ Clothing store");
+// document.write(userName+" "+"ordered 5"+" "+productTitle+" "+"on"+" "+brand)
